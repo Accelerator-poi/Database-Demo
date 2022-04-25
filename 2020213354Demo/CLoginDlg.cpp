07d96eb8d2ef54c44cd7c6941d4982d71952b148
Accelerator <2435184615@qq.com>
2022-04-25 23:40:09 +0800

    完成了注册功能与登录功能的部分实现，但是在注册时，同一套账号密码能够重复注册，有待于下一步解决

--- 2020213354Demo/CLoginDlg.cpp
+++ 2020213354Demo/CLoginDlg.cpp
@@ -1,6 +1,7 @@
 ﻿// CLoginDlg.cpp: 实现文件
 //
 
+#include<vector>
 #include "pch.h"
 #include "2020213354Demo.h"
 #include "afxdialogex.h"
@@ -43,6 +44,41 @@ END_MESSAGE_MAP()
 void CLoginDlg::OnBnClickedOk()
 {
 	// TODO: 在此添加控件通知处理程序代码
+
+	CString user_account, user_password;//用以保存用户输入的值
+	int IsFind = 0;//记录该用户名与密码是否在数据库中找到
+	m_edtUser.GetWindowText(user_account);
+	m_edtPassword.GetWindowText(user_password);
+
+	if (user_account != _T("") && user_password != _T(""))
+	{
+		std::vector<CString>name;
+		std::vector<CString>password;//用于储存从my_set中获取的数据
+		CString temp_name, temp_password;//用于临时储存从my_set中获取的数据
+		CRecordset my_set(&my_db);//申明CRecordset类的对象，用于调取表格数据 
+		my_set.Open(AFX_DB_USE_DEFAULT_TYPE, _T("select * from userinformation"));//调取表格数据
+		std::vector<CString>::iterator it_name;
+		std::vector<CString>::iterator it_password;
+
+		int i = 0;
+		while (!my_set.IsEOF())//将information表里的所有数据储存到容器里
+		{
+			my_set.GetFieldValue((short)0, temp_name);//取第1个属性
+			my_set.GetFieldValue((short)1, temp_password);//取第2个属性
+			name.push_back(temp_name);
+			password.push_back(temp_password);
+
+			my_set.MoveNext();//数据指针下移一行
+			i++;
+		}
+
+
+	}
+
+	else
+	{
+		AfxMessageBox(_T("请输入账号与密码！"));
+	}
 	CDialogEx::OnOK();
 }
 
@@ -62,6 +98,42 @@ void CLoginDlg::OnBnClickedCancel()
 void CLoginDlg::OnBnClickedEditRegister()
 {
 	// TODO: 在此添加控件通知处理程序代码
+	CString user_account, user_password;//用以保存用户输入的值
+	m_edtUser.GetWindowText(user_account);
+	m_edtPassword.GetWindowText(user_password);
+
+	if (user_account != _T("") && user_password != _T(""))//确保用户输入了账号与密码
+	{
+		CString mysql;
+		mysql.Format(_T("INSERT INTO userinformation VALUES(gs_encrypt_aes128('%s','Asdf1234'),gs_encrypt_aes128('%s','Asdf1234'));"), user_account, user_password);
+
+		//AfxMessageBox(mysql);//测试生成的sql语言，如果添加数据失败，可打开这个语句查看
+				//SQL代码是否符合语法
+
+		try
+		{
+			my_db.ExecuteSQL(mysql);
+		}
+		catch (CDBException* pe)
+		{
+			//如果有异常发生，弹出错误消息框，帮助纠正bug
+			pe->ReportError();
+			pe->Delete();
+		}
+
+		AfxMessageBox(_T("注册成功！"));
+
+		//清除输入框
+		m_edtUser.SetSel(0, -1);
+		m_edtUser.Clear();
+		m_edtPassword.SetSel(0, -1);
+		m_edtPassword.Clear();
+	}
+
+	else
+	{
+		AfxMessageBox(_T("请输入账号与密码！"));
+	}
 }
 
 
